750a4893b5efc42930415300c5a493b71b98b8f6
嘉川素子 <19motoko980430@gmail.com>
2024-03-17 18:12:43 +0900

    アコーディオンメニュー実装完了

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -19,12 +19,6 @@
     <link rel="apple-touch-icon-precomposed" href="画像のURL" />
     <!--OGPタグ/twitterカード-->
 
-    <!-- jsを使用 -->
-    <!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> -->
-    <!-- ↓この記述でつなげている -->
-    <!-- <script src="../../../public/js/script.js"></script> -->
-     <!-- <script src="{{ asset('js/script.js') }}"></script> -->
 
     <!-- アコーディオンメニューcssとjavascript　(bootstrap) -->
       <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -109,10 +103,9 @@
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <script src="{{ asset('js/script.js') }}"></script>
     <!-- ↓この記述でつなげている -->
     <!-- <script src="../../../public/js/script.js"></script> -->
-     <!-- <script src="{{ asset('js/script.js') }}"></script> -->
+     <script src="{{ asset('js/script.js') }}"></script>
 
 </body>
 </html>
