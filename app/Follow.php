8c7be3be177a261fe97a5e6a9623e8a385b9260e
嘉川素子 <19motoko980430@gmail.com>
2024-03-17 23:11:12 +0900

    【サイドバー】フォロー/フォロワー数を表示する

--- app/Follow.php
+++ app/Follow.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follow extends Model
 {
-    //
+
+    // リレーションの定義
+// public function follows(){
+//   return $this->hasMany('App/Follow');
+// }
+
+// フォロー数
+public function follows(){
+return $this->belongsToMany(User::class,'follows','following_id','followed_id');
+}
+// フォロワー数
+public function followers(){
+    return $this->belongsToMany(User::class,'follows','followed_id','following_id');
+}
+
 }
