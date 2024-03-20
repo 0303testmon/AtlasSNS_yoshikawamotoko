cddf1eb5350b237cbf56f30f7fb7479b29116ed9
嘉川素子 <19motoko980430@gmail.com>
2024-03-20 21:02:30 +0900

    投稿フォームを設置する

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,5 +2,17 @@
 
 @section('content')
 <h2>機能を実装していきましょう。</h2>
+<form class="post" method="POST">
+
+<div>
+  <a>
+    <img src="images/icon1.png">
+  </a>
+  <textarea class="post-main" name="post" placeholder="投稿内容を入力してください。" width="1000px"></textarea>
+</div>
+<button class="post-img">
+<img src="images/post.png" width="50px" height="50px">
+</button>
+</form>
 
 @endsection
