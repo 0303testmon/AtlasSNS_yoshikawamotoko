08f9cbe49b015b80b08158bdba96e34e5f71f91f
嘉川素子 <19motoko980430@gmail.com>
2024-05-04 13:38:29 +0900

    プロフィールの編集処理を実装する

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,16 +20,28 @@ class UsersController extends Controller
     //プロフィール編集機能
     public function updateProfile(Request $request){
 
+
+             //バリデーション
+        $request->validate([
+            'username' => 'required | between:2,12',
+            'mail' => 'required | between:5,40 | unique:users',
+            'password' => 'required | alpha_dash | between:8,20 | confirmed' ,
+            'bio' => 'between:0,150',
+            'file' => 'file|mimes:jpg,jpeg,png',
+        ]);
+
     $id = $request->input('id');
     $username = $request->input('username');
     $mail = $request->input('mail');
     $password = $request->input('password');
     $bio = $request->input('bio');
 
+
     User::where('id', $id)->update([
         'username' => $username,
         'mail' => $mail,
-        'password' => Hash::make($request->password),//ハッシュ化
+        'password' => $password,
+        // 'password' => Hash::make($request->password),//ハッシュ化
         'bio' => $bio,
     ]);
 
--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -9,27 +9,27 @@
     <img class="update-icon" src="images/icon1.png">
     <div class="update-form">
       <div class="update-block"><!--ユーザー名-->
-        <label for="name">user name</label>
+        <label for="name">ユーザー名</label>
         <input type="text" name="username" value="{{Auth::user()->username}}"><!--ログインユーザの情報をvalueを使って初期値に設定-->
       </div>
       <div class="update-block"><!--メールアドレス-->
-        <label for="mail">mail address</label>
+        <label for="mail">メールアドレス</label>
         <input type="email" name="mail" value="{{Auth::user()->mail}}">
       </div>
       <div class="update-block"><!--パスワード-->
-        <label for="pass">password</label>
+        <label for="pass">パスワード</label>
         <input type="password" name="password" value="">
       </div>
       <div class="update-block"><!--パスワード確認用-->
-        <label for="name">password confirm</label>
+        <label for="name">パスワード確認</label>
         <input type="password" name="password_confirmation" value="">
       </div>
       <div class="update-block"><!--自己紹介-->
-        <label for="name">bio</label>
+        <label for="name">自己紹介</label>
         <input type="text" name="bio" value="{{Auth::user()->bio}}">
       </div>
       <div class="update-block"><!--アイコン画像アップロード-->
-        <label for="name">icon image</label>
+        <label for="name">アイコン画像</label>
         <input type="file" name="images" >
       </div>
       <input type="submit" class="btn btn-danger">
