5fe2976978a9ec0f1ad527bc688359a0f1d35995
嘉川素子 <19motoko980430@gmail.com>
2024-05-26 21:06:46 +0900

    【サイドバー】フォロー/フォロワーリストページへ遷移するリンクを設置する

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -12,9 +12,10 @@ class FollowsController extends Controller
     //
     public function followList(){
 
-        $following_id = Auth::user()->pluck('followed_id');
-        $posts = Post::with('user')->whereIn('user_id', $following_id)->latest()->get();
-        return view('follows.followList', ['follows' => $follows,'posts' => $posts]);
+    //     $following_id = Auth::user()->pluck('followed_id');
+    //     $posts = Post::with('user')->whereIn('user_id', $following_id)->latest()->get();
+    //     return view('follows.followList', ['follows' => $follows,'posts' => $posts]);
+        return view('follows.followList');
     }
     public function followerList(){
         return view('follows.followerList');
